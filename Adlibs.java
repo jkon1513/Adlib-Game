9754d2a502e4ce4e560ab1c02a99d4ac430ea605
jkon1513 <jkon1513@gmail.com>
2018-03-20 23:03:27 -0400

    Create Adlibs.java

---
+++ Adlibs.java
@@ -0,0 +1,21 @@
+public class Adlibs{
+	
+	private Hashmap<String,String > options = new Hashmap<>();
+
+	public static void main(String[] args){
+	    
+	    initMap(options);
+	    
+
+	    // intitial instructions and welcome message
+	    System.out.println("Welcome to Adlibs!!\nThe way this works is a prompt will pop up and ask you to choose 
+      different story elements. you might have to name a character, or choose a verb, noun, or adjective.\n
+      when prompted please insert word that matches what is asked for.\n
+      HAVE FUN!!!\n\n");
+
+    }
+
+    private static initMap(Hashmap<String, String> options){
+        
+    }
+}
