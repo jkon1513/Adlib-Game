3c5311c4d12ee8fc2e3c90325ce7efd70f732fb1
Jkon1513 <jkon1513@gmail.com>
2018-03-23 23:46:28 -0400

    fixed welcome screen formatting

--- Adlibs.java
+++ Adlibs.java
@@ -1,18 +1,24 @@
-public class Adlibs{
-    private Hashmap<String,String > options = new Hashmap<>();
+//options: a hashmap with Key = type of word, value = word to be stored
 
+
+
+public class Adlibs{
+    
     public static void main(String[] args){
-        initMap(options);
 	    
 	// intitial instructions and welcome message
-	System.out.println("Welcome to Adlibs!!\nThe way this works is a prompt will pop up and ask you to choose 
-        different story elements. you might have to name a character, or choose a verb, noun, or adjective.\n
-        when prompted please insert word that matches what is asked for.\n
-        HAVE FUN!!!\n\n");
+	System.out.printf(
+	    "                       Welcome to Adlibs!!\n\n"+
+	    "The way this works is a prompt will pop up and ask you to choose"+ 
+	    " different story elements.you might have to name a character,"+
+	    "or choose a verb, noun, or adjective.\n"+
+            "When prompted,please insert a word that matches what is asked for.\n\n"+
+            "                           HAVE FUN!!!\n\n"
+	);
+
+
 
     }
 
-    private static initMap(Hashmap<String, String> options){
-        
-    }
+   
 }
